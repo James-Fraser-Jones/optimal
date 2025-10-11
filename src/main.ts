074aef1b2911e286ef940e0b04be5839d6fc9ff1
James Fraser-Jones <james@fraser-jones.co.uk>
2025-10-11 01:53:22 +0100

    added text box

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import initializeMatter from "./matter";
 import "./style.css";
 
+document.addEventListener("DOMContentLoaded", function (this: Document) {
+  this.getElementById("expression-input")!.addEventListener(
+    "input",
+    function (this: HTMLInputElement) {
+      this.value = this.value.replace(/\\/g, "\u03BB");
+    }
+  );
+});
+
 initializeMatter();
