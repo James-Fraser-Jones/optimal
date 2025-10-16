002d6851a1f0b9b7e0ac729d189cab16dc9788b8
James Fraser-Jones <james@fraser-jones.co.uk>
2025-10-16 23:07:19 +0100

    improved styling

--- src/codemirror.ts
+++ src/codemirror.ts
@@ -1,7 +1,37 @@
 import { basicSetup } from "codemirror";
-import { EditorSelection } from "@codemirror/state";
-import { EditorView } from "@codemirror/view";
+import { EditorSelection, type Extension } from "@codemirror/state";
+import {
+  Decoration,
+  EditorView,
+  MatchDecorator,
+  ViewPlugin,
+  scrollPastEnd,
+} from "@codemirror/view";
 import { addExpression } from "./matter";
+import { oneDark } from "@codemirror/theme-one-dark";
+
+function regexClassifier(regex: string, className: string): Extension {
+  const matcherDecorator = new MatchDecorator({
+    regexp: new RegExp(regex, "g"),
+    decoration: () => Decoration.mark({ class: className }),
+  });
+  return ViewPlugin.define(
+    (view) => ({
+      decorations: matcherDecorator.createDeco(view),
+      update(update) {
+        this.decorations = matcherDecorator.updateDeco(
+          update,
+          this.decorations
+        );
+      },
+    }),
+    {
+      decorations: (v) => v.decorations,
+    }
+  );
+}
+const highlightParens = regexClassifier(`\\(|\\)`, "token-paren");
+const highlightBinders = regexClassifier(`λ|\\.`, "token-binder");
 
 const lambdaExtension = EditorView.inputHandler.of((view, from, to, text) => {
   if (text === "\\") {
@@ -22,13 +52,31 @@ const changeListener = EditorView.updateListener.of((update) => {
   }
 });
 
+const largerFontTheme = EditorView.theme({
+  "&.cm-editor": {
+    fontSize: "20px",
+    height: "100%",
+  },
+});
+
 let editorView: EditorView;
 
 export function initializeCodeMirror() {
-  const input = "(λx.x)(λy.y)";
+  const newlines = "\n".repeat(30);
+  let input = "(λx.x)(λy.y)";
+  input += newlines;
   editorView = new EditorView({
     doc: input,
-    extensions: [basicSetup, lambdaExtension, changeListener],
+    extensions: [
+      basicSetup,
+      lambdaExtension,
+      changeListener,
+      scrollPastEnd(),
+      oneDark,
+      largerFontTheme,
+      highlightParens,
+      highlightBinders,
+    ],
     parent: document.getElementById("codemirror")!,
   });
   addExpression(input);
