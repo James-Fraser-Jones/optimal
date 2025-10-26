c52f76e9a9dde61f5896749fa6ace7757ea67036
James Fraser-Jones <james@fraser-jones.co.uk>
2025-10-26 01:21:00 +0100

    add cooler d3 example

--- src/d3.ts
+++ src/d3.ts
@@ -1,43 +1,101 @@
 import * as d3 from "d3";
+import dataJson from "../d3data.json";
+
+interface Node extends d3.SimulationNodeDatum {
+  id: string;
+  group: string;
+  radius: number;
+  citing_patents_count: number;
+}
+
+interface Link extends d3.SimulationLinkDatum<Node> {
+  source: string | Node;
+  target: string | Node;
+  value: number;
+}
+
+interface GraphData {
+  nodes: Node[];
+  links: Link[];
+}
 
 export function initializeD3() {
+  //setting up the svg canvas
   const parent = document.getElementById("d3")!;
-
-  // Declare the chart dimensions and margins.
   const width = parent.clientWidth;
   const height = parent.clientHeight;
-  const marginTop = 20;
-  const marginRight = 20;
-  const marginBottom = 30;
-  const marginLeft = 40;
-
-  // Declare the x (horizontal position) scale.
-  const x = d3
-    .scaleUtc()
-    .domain([new Date("2023-01-01"), new Date("2024-01-01")])
-    .range([marginLeft, width - marginRight]);
-
-  // Declare the y (vertical position) scale.
-  const y = d3
-    .scaleLinear()
-    .domain([0, 100])
-    .range([height - marginBottom, marginTop]);
-
-  // Create the SVG container.
-  const svg = d3.create("svg").attr("width", width).attr("height", height);
-
-  // Add the x-axis.
-  svg
-    .append("g")
-    .attr("transform", `translate(0,${height - marginBottom})`)
-    .call(d3.axisBottom(x));
-
-  // Add the y-axis.
-  svg
-    .append("g")
-    .attr("transform", `translate(${marginLeft},0)`)
-    .call(d3.axisLeft(y));
-
-  // Append the SVG element.
+  const svg = d3
+    .create("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [-width / 2, -height / 2, width, height])
+    .attr("style", "max-width: 100%; height: auto;");
   parent.append(svg.node()!);
+
+  //setting up the simulation
+  const data: GraphData = dataJson as GraphData;
+  const simulation = d3
+    .forceSimulation(data.nodes)
+    .force(
+      "link",
+      d3.forceLink(data.links).id((d) => (d as Node).id)
+    )
+    .force("charge", d3.forceManyBody())
+    .force("x", d3.forceX())
+    .force("y", d3.forceY());
+  simulation.on("tick", () => {
+    link
+      .attr("x1", (d) => (d.source as Node).x!)
+      .attr("y1", (d) => (d.source as Node).y!)
+      .attr("x2", (d) => (d.target as Node).x!)
+      .attr("y2", (d) => (d.target as Node).y!);
+    node.attr("cx", (d) => (d as Node).x!).attr("cy", (d) => (d as Node).y!);
+  });
+
+  //drawing svg elements
+  const color = d3.scaleOrdinal(d3.schemeCategory10);
+  const link = svg
+    .append("g")
+    .attr("stroke", "#999")
+    .attr("stroke-opacity", 0.6)
+    .selectAll("line")
+    .data(data.links)
+    .join("line")
+    .attr("stroke-width", (d) => Math.sqrt(d.value));
+  const node = svg
+    .append("g")
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 1.5)
+    .selectAll("circle")
+    .data(data.nodes)
+    .join("circle")
+    .attr("r", 5)
+    .attr("fill", (d) => color(d.group));
+
+  //adding title elements
+  node.append("title").text((d) => d.id);
+
+  //drag functionality
+  node.call(
+    (d3 as any)
+      .drag()
+      .on("start", (event: any) => {
+        if (!event.active) {
+          simulation.alphaTarget(0.3).restart();
+        }
+        event.subject.fx = event.subject.x;
+        event.subject.fy = event.subject.y;
+      })
+      .on("drag", (event: any) => {
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
+      })
+      .on("end", (event: any) => {
+        if (!event.active) {
+          simulation.alphaTarget(0);
+        }
+        event.subject.fx = null;
+        event.subject.fy = null;
+      })
+  );
 }
